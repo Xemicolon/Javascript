f1281c80740f218cfeb89a32db35f29cc11d422b
Xemicolon <1vibenation@gmail.com>
2020-04-18 22:48:58 +0100

    Javascript Task 3

--- Task3/index.js
+++ Task3/index.js
@@ -7,32 +7,30 @@ function yugioh(num) {
   let three = "gi";
   let five = "oh";
   // if our conditions below is true, we push the results to our array
-    
-    // if (Number.isInteger(num)) {
-    //     console.log(num)
-    // } else {
-    //     console.log('not a number')
-    // }
-  
-  for (let i = 1; i <= num; i++) {
-    if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
-      arr.push(`${two}-${three}-${five}`);
-    } else if (i % 2 === 0 && i % 3 === 0) {
-      arr.push(`${two}-${three}`);
-    } else if (i % 2 === 0 && i % 5 === 0) {
-      arr.push(`${two}-${five}`);
-    } else if (i % 2 === 0) {
-      arr.push(`${two}`);
-    } else if (i % 3 === 0) {
-      arr.push(`${three}`);
-    } else if (i % 5 === 0) {
-      arr.push(`${five}`);
-    } else {
-      arr.push(i);
+
+  if (Number.isInteger(num)) {
+    for (let i = 1; i <= num; i++) {
+      if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+        arr.push(`${two}-${three}-${five}`);
+      } else if (i % 2 === 0 && i % 3 === 0) {
+        arr.push(`${two}-${three}`);
+      } else if (i % 2 === 0 && i % 5 === 0) {
+        arr.push(`${two}-${five}`);
+      } else if (i % 2 === 0) {
+        arr.push(`${two}`);
+      } else if (i % 3 === 0) {
+        arr.push(`${three}`);
+      } else if (i % 5 === 0) {
+        arr.push(`${five}`);
+      } else {
+        arr.push(i);
+      }
     }
+    // then return our final results in our array 😁😎
+    return arr;
+  } else {
+    return "Please, enter a whole number/integer";
   }
-  // then return our final results in our array 😁😎
-  return arr;
 }
 
 let myNum = yugioh(100);
